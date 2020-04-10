7ae693dc87f2afd59d818748a4546a602b8afcc2
Kendall Hunt <hunt.kendall12@gmail.com>
2020-04-09 22:47:05 -0400

    Adjustments
    Made adjustments to all files. Only things that need finished are aligning the caption for the photo within the photo, and making the "party time" button reactive

--- LOL.js
+++ LOL.js
@@ -22,3 +22,32 @@ function displayClock() {
 }
 
 displayClock();
+
+// write in function that changes the picture based on the time in the displayClock() function
+function pictureGenerator() {
+var catPicture = document.getElementById('catPicture');
+const catPictureSpan = document.getElementById('randomCatPic');
+const currentTime = new Date();
+var hour = currentTime.getHours();
+
+   if (hour >= 7 && hour < 12) {
+    catPicture.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    catPictureSpan.innerText = "Good Morning! Hope You enjoyed your cat nap!";
+}  else if (hour >= 12 && hour < 2) {
+    catPicture.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+    catPictureSpan.innerText = "It's lunch time kitties!";
+}  else if (hour >= 14 && hour < 16) {
+    catPicture.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+    catPictureSpan.innerText = "Let's relax and take another cat nap.";
+}  else if (hour >= 16 && hour < 18) {
+    catPicture.src = 'https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg';
+    catPictureSpan.innerText = "IT'S PARTY TIIIME!"
+}  else {
+    catPicture.src = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
+    catPictureSpan.innerText = "Let's go to sleep for the night.";
+}
+};
+
+pictureGenerator();
+
+// add a caption to the picture based on the time in the displayClock() function
